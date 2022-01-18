c6e4bad1bf8d8fd6a75aa4ceffa3c703dcaab3bf
justin <justinschwerdtfeger245@gmail.com>
2022-01-17 18:14:00 -0600

    now it hsould have all the stuff

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/advanced/CyclingRed.kt
@@ -0,0 +1,234 @@
+package org.firstinspires.ftc.teamcode.auto.advanced
+
+import com.acmerobotics.roadrunner.geometry.Vector2d
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import org.firstinspires.ftc.teamcode.auto.*
+import org.firstinspires.ftc.teamcode.drive.DriveConstants
+import org.firstinspires.ftc.teamcode.drive.FrequentPositions.cycleDumpPosition
+import org.firstinspires.ftc.teamcode.drive.FrequentPositions.duckLocation
+import org.firstinspires.ftc.teamcode.drive.FrequentPositions.dumpPosition
+import org.firstinspires.ftc.teamcode.drive.FrequentPositions.startingPosition
+import org.firstinspires.ftc.teamcode.drive.Robot
+import org.firstinspires.ftc.teamcode.drive.Robot.getVelocityConstraint
+import org.firstinspires.ftc.teamcode.modules.capstone.Capstone
+import org.firstinspires.ftc.teamcode.modules.carousel.Carousel
+import org.firstinspires.ftc.teamcode.modules.deposit.Deposit
+import org.firstinspires.ftc.teamcode.modules.intake.Intake
+import org.firstinspires.ftc.teamcode.modules.vision.Detector
+import org.firstinspires.ftc.teamcode.trajectorysequenceimproved.TrajectorySequence
+import org.firstinspires.ftc.teamcode.util.field.Alliance
+import org.firstinspires.ftc.teamcode.util.field.Context.location
+import org.firstinspires.ftc.teamcode.util.field.Context.side
+import org.firstinspires.ftc.teamcode.util.field.OpModeType
+import org.firstinspires.ftc.teamcode.util.field.Side
+import org.firstinspires.ftc.teamcode.roadrunnerext.flip
+import kotlin.Throws
+
+@Autonomous
+class CyclingRed : LinearOpMode() {
+    lateinit var robot: Robot
+    lateinit var deposit: Deposit
+    lateinit var intake: Intake
+    lateinit var capstone: Capstone
+    lateinit var carousel: Carousel
+    private val blue = false
+
+    @Throws(InterruptedException::class)
+    override fun runOpMode() {
+        side = Side.CYCLING
+        robot = Robot(this, startingPosition(), OpModeType.AUTO, Alliance.RED)
+        intake = robot.intake
+        capstone = robot.capstone
+        deposit = robot.deposit
+        carousel = robot.carousel
+        robot.visionInit()
+        val leftSequence = leftAuto()
+        val middleSequence = middleAuto()
+        val rightSequence = rightAuto()
+        while (!opModeIsActive()) {
+            robot.scan()
+            telemetry.addData("Position", location)
+            telemetry.update()
+        }
+        waitForStart()
+        val sequence = when (location) {
+            Detector.Location.LEFT -> leftSequence
+            Detector.Location.MIDDLE -> middleSequence
+            Detector.Location.RIGHT -> rightSequence
+        }
+        robot.turnOffVision()
+        robot.followTrajectorySequence(sequence)
+    }
+    fun leftAuto() : TrajectorySequence {
+        val trajectoryBuilder =
+            robot.trajectorySequenceBuilder(startingPosition())
+                .setReversed(true)
+                .capstoneReady(capstone)
+                .setVelConstraint(getVelocityConstraint(30.0, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH))
+                .splineTo(
+                    duckLocation(Detector.Location.LEFT).vec(),
+                    Math.toRadians(90.0).flip(blue) + duckLocation().heading
+                )
+                .resetConstraints()
+                .capstonePickup(capstone)
+                .liftUp(deposit, Robot.getLevel(Detector.Location.LEFT))
+                .waitWhile(capstone::isDoingWork) // capstone loaded
+                .splineTo(dumpPosition().vec(), Math.PI.flip(blue) + dumpPosition().heading)
+                .setReversed(false)
+                .dump(deposit)
+                .waitWhile(deposit::isDoingWork) // wait for platform to dumpPosition
+        for (i in 1..1)
+            trajectoryBuilder
+                .UNSTABLE_addDisplacementMarkerOffset(10.0) {
+                    intake.setPower(1.0)
+                }
+                .splineTo(
+                    Vector2d(20.0, -40.0).flip(blue), 0.0
+                )
+                .increaseGains()
+                .splineTo(
+                    Vector2d(24.0, -40.0).flip(blue), 0.0
+                )
+                .defaultGains()
+                .splineTo(
+                    Vector2d(52.0, -50.0).plus(
+                        Vector2d(
+                            5 * Math.random(),
+                            5 * Math.random()
+                        )
+                    ).flip(blue), Math.toRadians(-35.0 + 5 * Math.random()).flip(blue)
+                )
+                .setReversed(true)
+                .intakeOff(intake)
+                .splineTo(
+                    Vector2d(26.0, -40.0).flip(blue), Math.PI
+                )
+                .increaseGains()
+                .splineTo(
+                    Vector2d(24.0, -40.0).flip(blue), Math.PI
+                )
+                .defaultGains()
+                .splineTo(cycleDumpPosition().vec(), cycleDumpPosition().heading + Math.PI.flip(blue))
+                .dump(deposit)
+                .waitWhile(deposit::isDoingWork) // wait for platform to dumpPosition
+                .setReversed(false)
+        return trajectoryBuilder
+            .splineTo(Vector2d(45.0, -45.0).flip(blue), Math.toRadians(-35.0).flip(blue))
+            .build()
+    }
+    fun middleAuto() : TrajectorySequence {
+        val trajectoryBuilder =
+            robot.trajectorySequenceBuilder(startingPosition())
+                .setReversed(true)
+                .capstoneReady(capstone)
+                .setVelConstraint(getVelocityConstraint(30.0, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH))
+                .splineTo(
+                    duckLocation(Detector.Location.MIDDLE).vec(),
+                    Math.toRadians(90.0).flip(blue) + duckLocation().heading
+                )
+                .resetConstraints()
+                .capstonePickup(capstone)
+                .liftUp(deposit, Robot.getLevel(Detector.Location.MIDDLE))
+                .waitWhile(capstone::isDoingWork) // capstone loaded
+                .splineTo(dumpPosition().vec(), Math.PI.flip(blue) + dumpPosition().heading)
+                .setReversed(false)
+                .dump(deposit)
+                .waitWhile(deposit::isDoingWork) // wait for platform to dumpPosition
+        for (i in 1..1)
+            trajectoryBuilder
+                .UNSTABLE_addDisplacementMarkerOffset(10.0) {
+                    intake.setPower(1.0)
+                }
+                .splineTo(
+                    Vector2d(20.0, -40.0).flip(blue), 0.0
+                )
+                .increaseGains()
+                .splineTo(
+                    Vector2d(24.0, -40.0).flip(blue), 0.0
+                )
+                .defaultGains()
+                .splineTo(
+                    Vector2d(52.0, -50.0).plus(
+                        Vector2d(
+                            5 * Math.random(),
+                            5 * Math.random()
+                        )
+                    ).flip(blue), Math.toRadians(-35.0 + 5 * Math.random()).flip(blue)
+                )
+                .setReversed(true)
+                .intakeOff(intake)
+                .splineTo(
+                    Vector2d(26.0, -40.0).flip(blue), Math.PI
+                )
+                .increaseGains()
+                .splineTo(
+                    Vector2d(24.0, -40.0).flip(blue), Math.PI
+                )
+                .defaultGains()
+                .splineTo(cycleDumpPosition().vec(), cycleDumpPosition().heading + Math.PI.flip(blue))
+                .dump(deposit)
+                .waitWhile(deposit::isDoingWork) // wait for platform to dumpPosition
+                .setReversed(false)
+        return trajectoryBuilder
+            .splineTo(Vector2d(45.0, -45.0).flip(blue), Math.toRadians(-35.0).flip(blue))
+            .build()
+    }
+    fun rightAuto() : TrajectorySequence {
+        val trajectoryBuilder =
+            robot.trajectorySequenceBuilder(startingPosition())
+                .setReversed(true)
+                .capstoneReady(capstone)
+                .setVelConstraint(getVelocityConstraint(30.0, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH))
+                .splineTo(
+                    duckLocation(Detector.Location.RIGHT).vec(),
+                    Math.toRadians(90.0).flip(blue) + duckLocation().heading
+                )
+                .resetConstraints()
+                .capstonePickup(capstone)
+                .liftUp(deposit, Robot.getLevel(Detector.Location.RIGHT))
+                .waitWhile(capstone::isDoingWork) // capstone loaded
+                .splineTo(dumpPosition().vec(), Math.PI.flip(blue) + dumpPosition().heading)
+                .setReversed(false)
+                .dump(deposit)
+                .waitWhile(deposit::isDoingWork) // wait for platform to dumpPosition
+        for (i in 1..1)
+            trajectoryBuilder
+                .UNSTABLE_addDisplacementMarkerOffset(10.0) {
+                    intake.setPower(1.0)
+                }
+                .splineTo(
+                    Vector2d(20.0, -40.0).flip(blue), 0.0
+                )
+                .increaseGains()
+                .splineTo(
+                    Vector2d(24.0, -40.0).flip(blue), 0.0
+                )
+                .defaultGains()
+                .splineTo(
+                    Vector2d(52.0, -50.0).plus(
+                        Vector2d(
+                            5 * Math.random(),
+                            5 * Math.random()
+                        )
+                    ).flip(blue), Math.toRadians(-35.0 + 5 * Math.random()).flip(blue)
+                )
+                .setReversed(true)
+                .intakeOff(intake)
+                .splineTo(
+                    Vector2d(26.0, -40.0).flip(blue), Math.PI
+                )
+                .increaseGains()
+                .splineTo(
+                    Vector2d(24.0, -40.0).flip(blue), Math.PI
+                )
+                .defaultGains()
+                .splineTo(cycleDumpPosition().vec(), cycleDumpPosition().heading + Math.PI.flip(blue))
+                .dump(deposit)
+                .waitWhile(deposit::isDoingWork) // wait for platform to dumpPosition
+                .setReversed(false)
+        return trajectoryBuilder
+            .splineTo(Vector2d(45.0, -45.0).flip(blue), Math.toRadians(-35.0).flip(blue))
+            .build()
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/DriverPractice.java
@@ -0,0 +1,153 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.arcrobotics.ftclib.gamepad.ButtonReader;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.arcrobotics.ftclib.gamepad.KeyReader;
+import com.arcrobotics.ftclib.gamepad.ToggleButtonReader;
+import com.arcrobotics.ftclib.gamepad.TriggerReader;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.drive.Robot;
+import org.firstinspires.ftc.teamcode.modules.capstone.Capstone;
+import org.firstinspires.ftc.teamcode.modules.carousel.Carousel;
+import org.firstinspires.ftc.teamcode.modules.deposit.Deposit;
+import org.firstinspires.ftc.teamcode.modules.intake.Intake;
+import org.firstinspires.ftc.teamcode.util.field.Balance;
+import org.firstinspires.ftc.teamcode.util.field.Context;
+import org.firstinspires.ftc.teamcode.util.field.OpModeType;
+
+import static org.firstinspires.ftc.teamcode.util.field.Context.balance;
+
+@TeleOp
+public class DriverPractice extends LinearOpMode {
+    Robot robot;
+    Intake intake;
+    Deposit deposit;
+    Carousel carousel;
+    Capstone capstone;
+
+    GamepadEx primary;
+    GamepadEx secondary;
+    KeyReader[] keyReaders;
+    TriggerReader intakeButton, ninjaMode, liftButton;
+    ButtonReader levelIncrement, levelDecrement, dumpButton, tippedToward, tippedAway,
+            capstoneReady, capstoneDrop, capstoneIn;
+    ToggleButtonReader carouselButton;
+
+    Deposit.State defaultDepositState = Deposit.State.LEVEL3;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        robot = new Robot(this, OpModeType.TELE);
+        intake = robot.intake;
+        deposit = robot.deposit;
+        carousel = robot.carousel;
+        capstone = robot.capstone;
+        primary = new GamepadEx(gamepad1);
+        secondary = new GamepadEx(gamepad2);
+        keyReaders = new KeyReader[] {
+                capstoneReady = new ButtonReader(primary, GamepadKeys.Button.B),
+                capstoneDrop = new ButtonReader(primary, GamepadKeys.Button.A),
+                capstoneIn = new ButtonReader(primary, GamepadKeys.Button.X),
+                intakeButton = new TriggerReader(secondary, GamepadKeys.Trigger.RIGHT_TRIGGER),
+                ninjaMode = new TriggerReader(primary, GamepadKeys.Trigger.LEFT_TRIGGER),
+                levelIncrement = new ButtonReader(secondary, GamepadKeys.Button.DPAD_UP),
+                levelDecrement = new ButtonReader(secondary, GamepadKeys.Button.DPAD_DOWN),
+                liftButton = new TriggerReader(secondary, GamepadKeys.Trigger.LEFT_TRIGGER),
+                tippedAway = new ButtonReader(secondary, GamepadKeys.Button.LEFT_BUMPER),
+                tippedToward = new ButtonReader(secondary, GamepadKeys.Button.RIGHT_BUMPER),
+                carouselButton = new ToggleButtonReader(primary, GamepadKeys.Button.LEFT_BUMPER),
+                dumpButton = new ButtonReader(primary, GamepadKeys.Button.RIGHT_BUMPER),
+        };
+        Deposit.farDeposit = true;
+        waitForStart();
+        while (opModeIsActive()) {
+            robot.update();
+            for (KeyReader reader : keyReaders) {
+                reader.readValue();
+            }
+            Pose2d drivePower = new Pose2d(
+                    -gamepad1.left_stick_y,
+                    0,
+                    gamepad1.right_stick_x
+            );
+            if (ninjaMode.isDown()) drivePower = drivePower.div(3);
+            robot.setWeightedDrivePower(drivePower);
+            setIntake();
+            setDeposit();
+            setCarousel();
+            Context.packet.addLine(intake.getState() + "");
+            Context.packet.addLine(defaultDepositState + "");
+            telemetry.addData("Intake State", intake.getState());
+            telemetry.addData("Default Height", defaultDepositState);
+            if (tippedAway.isDown() && tippedToward.isDown()) {
+                balance = Balance.BALANCED;
+            } else if (tippedAway.isDown()) {
+                balance = Balance.AWAY;
+            } else if (tippedToward.isDown()) {
+                balance = Balance.TOWARD;
+            }
+            setCapstone();
+        }
+    }
+
+    void setCapstone() {
+        if (capstoneDrop.wasJustPressed()) {
+            if (robot.capstone.getState() == Capstone.State.PRECAP) {
+                robot.capstone.cap();
+            } else {
+                robot.capstone.preCap();
+            }
+        } else if (capstoneIn.wasJustPressed()) {
+            robot.capstone.hold();
+        }
+    }
+
+    void setIntake() {
+        if (intakeButton.isDown()) {
+            intake.setPower(1);
+        } else {
+            intake.setPower(0);
+        }
+    }
+
+    void setDeposit() {
+        if (levelIncrement.wasJustPressed()) {
+            switch (defaultDepositState) {
+                case LEVEL2:
+                    defaultDepositState = Deposit.State.LEVEL3;
+                    break;
+                case IDLE:
+                    defaultDepositState = Deposit.State.LEVEL2;
+                    break;
+            }
+            deposit.setState(defaultDepositState);
+        } else if (levelDecrement.wasJustPressed()) {
+            switch (defaultDepositState) {
+                case LEVEL3:
+                    defaultDepositState = Deposit.State.LEVEL2;
+                    break;
+                case LEVEL2:
+                    defaultDepositState = Deposit.State.IDLE;
+                    break;
+            }
+            deposit.setState(defaultDepositState);
+        }
+
+        if (dumpButton.wasJustPressed()) {
+            deposit.dump();
+        }
+    }
+
+    void setCarousel() {
+        carousel.setPower(gamepad2.right_stick_y);
+//        if(carouselButton.getState()) {
+//            carousel.on();
+//        } else {
+//            carousel.off();
+//        }
+    }
+}
